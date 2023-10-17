1cf24e3d411b419c78c98a2688ed128998d2d5cc
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2023-10-17 13:02:01 +0200

    i have added comments to the code i have made

--- CVR-P5/Assets/Scripts/Action.cs
+++ CVR-P5/Assets/Scripts/Action.cs
@@ -2,25 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [Serializable]
-public class Action: MonoBehaviour
+public class Action : MonoBehaviour
 {
+    // Private field to store the default name for the action.
     [SerializeField]
     private string name = "_action";
 
-    public virtual void actionUpdate() {
+    /// <summary>
+    /// Virtual method for updating the action. Override this method in derived classes.
+    /// </summary>
+    public virtual void actionUpdate()
+    {
         return;
     }
 
+    /// <summary>
+    /// Virtual method for starting the action. Override this method in derived classes.
+    /// </summary>
     public virtual void startAction()
     {
         return;
     }
 
-    public string getActionName() {
+    /// <summary>
+    /// Method to get the name of the action.
+    /// </summary>
+    /// <returns></returns>
+    public string getActionName()
+    {
         return name;
     }
-    public virtual void endAction() {
-    
+
+    /// <summary>
+    /// Virtual method for ending the action. Override this method in derived classes if needed.
+    /// This method can be empty if ending the action doesn't require any specific logic.
+    /// </summary>
+    public virtual void endAction()
+    {
     }
 }
--- CVR-P5/Assets/Scripts/ActionManager.cs
+++ CVR-P5/Assets/Scripts/ActionManager.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Manages a list of actions and controls their execution.
+/// </summary>
 public class ActionManager : MonoBehaviour
 {
     [Header("actions")]
     [SerializeField]
-    Action[] listOfActions;
-    [SerializeField]
-    int currentActionIndex = -1;
-    [SerializeField]
-    bool actionsActive = true;
+    Action[] listOfActions; // Array to store different Action instances.
 
-    // Update is called once per frame
+    [SerializeField]
+    int currentActionIndex = -1; // Index of the currently active action.
+
+    [SerializeField]
+    bool actionsActive = true; // Flag indicating whether actions are active or paused.
+
+    /// <summary>
+    /// Update is called once per frame. Checks for active action and updates it.
+    /// </summary>
     void Update()
     {
-        if (currentActionIndex >= 0 && actionsActive) {
+        if (currentActionIndex >= 0 && actionsActive)
+        {
             listOfActions[currentActionIndex].actionUpdate();
         }
     }
 
+    /// <summary>
+    /// Ends the current action and starts a new one based on the provided action name.
+    /// </summary>
+    /// <param name="actionName">Name of the action to be started.</param>
     public void endAndStartNewActionWithSearch(string actionName)
     {
         for (int i = 0; i < listOfActions.Length; i++)
@@ -31,33 +43,62 @@ public class ActionManager : MonoBehaviour
             }
         }
     }
-    public void endAndStartNewAction(int actionIndex) {
+
+    /// <summary>
+    /// Ends the current action and starts a new one based on the provided index.
+    /// </summary>
+    /// <param name="actionIndex">Index of the action to be started.</param>
+    public void endAndStartNewAction(int actionIndex)
+    {
         endAction();
         runAction(actionIndex);
     }
 
+    /// <summary>
+    /// Ends the current action and disables all actions.
+    /// </summary>
     public void endAction()
     {
         listOfActions[currentActionIndex].endAction();
         disableAllActions();
     }
 
-    public void disableAllActions() {
+    /// <summary>
+    /// Disables all actions.
+    /// </summary>
+    public void disableAllActions()
+    {
         actionsActive = false;
     }
+
+    /// <summary>
+    /// Enables all actions.
+    /// </summary>
     public void enableAllActions()
     {
         actionsActive = true;
     }
-    public void runActionWithSearch(string actionName) {
-        for (int i = 0; i < listOfActions.Length; i++) {
-            if (actionName == listOfActions[i].getActionName()) {
+
+    /// <summary>
+    /// Runs the action with the provided name.
+    /// </summary>
+    /// <param name="actionName">Name of the action to be executed.</param>
+    public void runActionWithSearch(string actionName)
+    {
+        for (int i = 0; i < listOfActions.Length; i++)
+        {
+            if (actionName == listOfActions[i].getActionName())
+            {
                 runAction(i);
                 break;
             }
         }
     }
 
+    /// <summary>
+    /// Runs the action with the provided index.
+    /// </summary>
+    /// <param name="actionIndex">Index of the action to be executed.</param>
     public void runAction(int actionIndex)
     {
         currentActionIndex = actionIndex;
@@ -65,7 +106,11 @@ public class ActionManager : MonoBehaviour
         enableAllActions();
     }
 
-    public void runNextAction() {
-        runAction(currentActionIndex+1);
+    /// <summary>
+    /// Runs the next action in the list.
+    /// </summary>
+    public void runNextAction()
+    {
+        runAction(currentActionIndex + 1);
     }
 }
