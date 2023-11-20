c10d6b7dd3fbdcf259979716e23bca853831af4c
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2023-11-20 17:23:28 +0100

    HADDEDE TEXTURED ALIEN SPIDER

--- CVR-P5/Assets/PuzzelAction.cs
+++ CVR-P5/Assets/PuzzelAction.cs
@@ -11,22 +11,27 @@ public class PuzzelAction : MonoBehaviour
     [SerializeField]
     TMP_Text[] screenDigigts;
     [SerializeField]
-    int code = 121;
+    int code = 123;
     [SerializeField]
     int max = 3, min = 1;
     [SerializeField]
     public bool gotCode = false;
     public void pressButton(int index) {
-        if (gotCode)
+        if (gotCode) {
             return;
-        digigts[index]++;
-        if (digigts[index] < max) {
+        }
+            
+        digigts[index] += 1;
+        if (digigts[index] > max) {
             digigts[index] = min;
         }
         screenDigigts[index].text = digigts[index].ToString();
-        string currentCode = digigts[0].ToString() + digigts[0].ToString() + digigts[0].ToString();
+        string currentCode = digigts[0].ToString() + digigts[1].ToString() + digigts[2].ToString();
+        Debug.Log(currentCode  + " == " + code.ToString() + "["+(currentCode == code.ToString()) +"]");
         if (currentCode == code.ToString()) {
             gotCode = true;
+            codePart.SetActive(false);
+            finnishPar.SetActive(true);
         }
     }
     
