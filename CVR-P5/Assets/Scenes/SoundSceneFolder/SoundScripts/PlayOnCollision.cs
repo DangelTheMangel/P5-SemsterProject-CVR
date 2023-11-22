c7ed8e3753bb6c5cd8d61fc1ccb03b1e7c370fd0
BlueSlimeX <56292519+BlueSlimeX@users.noreply.github.com>
2023-11-22 15:29:42 +0100

    Collider Sound Test

---
+++ CVR-P5/Assets/Scenes/SoundSceneFolder/SoundScripts/PlayOnCollision.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnCollision : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Collider")
+        {
+            audioSource.PlayOneShot(clip, volume);
+        }
+
+    }
+
+
+}
