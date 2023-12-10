985d9761c082c1bce5bafd293b13e5187bf47a86
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2023-12-10 14:37:56 +0100

    Audiolog script done. can now play an ideal sound and when pressed it swichtes to thte audilog. have not implemenetede in main scene

--- CVR-P5/Assets/AudioLogController.cs
+++ CVR-P5/Assets/AudioLogController.cs
@@ -8,12 +8,18 @@ public class AudioLogController : MonoBehaviour
     public bool isGrabbed = false;
     public bool canPress = false;
     float canpressTimer = 1f;
+    [SerializeField]
+    AudioClip audiolog;
+    AudioClip idealSound;
+    [SerializeField]
+    bool isplaying = false;
     // Start is called before the first frame update
     void Start()
     {
         XRGrabInteractable xrGrab = GetComponent<XRGrabInteractable>();
         xrGrab.activated.AddListener(clickOnAL);
         audioSource = GetComponent<AudioSource>();
+        idealSound = audioSource.clip;
     }
 
     // Update is called once per frame
@@ -24,17 +30,28 @@ public class AudioLogController : MonoBehaviour
 
     void clickOnAL(ActivateEventArgs activateEventArgs) {
         if (canPress) { 
-            if (audioSource.isPlaying)
+            if (isplaying)
             {
-                audioSource.Stop();
+                audioSource.clip = idealSound;
+                audioSource.Play();
+                isplaying = false;
             }
             else {
+                audioSource.clip = audiolog;
                 audioSource.Play();
+                isplaying = true;
             }
         }
         
     }
+    public void focused() {
+        canPress = true;
+    }
 
+    public void notFocused() {
+        canPress = false;
+
+    }
     public void grabbed() {
         isGrabbed = !isGrabbed;
     }
